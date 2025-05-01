1239d8786f449e64397f05b80ca7cd591ea39cb8
Sl00WBro <107092631+Sl00WBro@users.noreply.github.com>
2025-05-01 21:05:37 +0500

    делал

--- WeatherCore/Form1.cs
+++ WeatherCore/Form1.cs
@@ -687,6 +687,7 @@ namespace WeatherCore
             //var condition = _conditions?.FirstOrDefault(c => c.code == code);
             //var ru = condition?.languages?.FirstOrDefault(l => l.lang_iso == "ru");
             // не работает переделывай
+            //?
         }
     }
 }
